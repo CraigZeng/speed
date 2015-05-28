e179729fb72a794bcafefc51c03587607e38227f
zengcheng <zengcheng@baijiahulian.com>
2015-05-28 21:02:20 +0800

    打包结构完成

--- demo/gulpfile.js
+++ demo/gulpfile.js
@@ -1,17 +1,65 @@
 var gulp = require('gulp');
+var path = require('path');
 var usemin = require('gulp-usemin');
 var uglify = require('gulp-uglify');
 var minifyHtml = require('gulp-minify-html');
 var minifyCss = require('gulp-minify-css');
 var rev = require('gulp-rev');
+var lessc = require('gulp-less');
 var deployStatic = require('gulp-deploy-static');
 var amdOptimize = require('gulp-amd-optimize');
+var through = require('through2');
+var gutil = require('gulp-util');
+
+var JS_STATIC_DIR = path.join('.', 'static', 'js');
+
+var rjs = function() {
+  return through.obj(function(file, enc, callback) {
+    if (file.isStream()) {
+      this.emit('error', new gutil.PluginError('gulp-deploy-static', 'Streams are not supported!'));
+      callback();
+    }
+    else if (file.isNull())
+      callback(null, file);
+    else {
+      try {
+        var resultPath = file.path;
+        var resultContent = '';
+        var that = this;
+        var stream = through.obj(function(file, enc, streamCallback) {
+            streamCallback(null, file);
+        });
+        var moduleName = path.relative(path.resolve(JS_STATIC_DIR), resultPath).slice(0, -3);
+        stream
+            .pipe(amdOptimize(moduleName, {wrapShim: true, baseUrl:JS_STATIC_DIR}))
+            .on('data', function(file) {
+                resultContent = resultContent + new String(file.contents);
+            })
+            .on('end', function(){
+                resultContent = resultContent
+                    + 'require(["' + moduleName + '"], function (mod) {'
+                    +       'mod.init();'
+                    + '});';
+                callback(null, new gutil.File({
+                  path: resultPath,
+                  contents: new Buffer(resultContent)
+                }));
+            });
+        stream.write(file);
+        stream.end();
+      } catch(e) {
+        this.emit('error', e);
+        callback();
+      }
+    }
+  });
+}
 
 gulp.task('usemin', function () {
   return gulp.src('./view/*.html')
       .pipe(usemin({
-        css: [minifyCss(), rev()],
-        js: [rev()],
+        css: [lessc(), 'concat', minifyCss(), rev()],
+        js: [rjs(), 'concat', uglify(), rev()],
         inlinejs: [uglify()],
         inlinecss: [minifyCss(), 'concat'],
         assetsDir : './'
@@ -21,15 +69,7 @@ gulp.task('usemin', function () {
             return 'http://www.baidu.com' + path;
         }
       }))
-      .pipe(gulp.dest('build/'))
-      .pipe(rev.manifest())
-      .pipe(gulp.dest('build/static/js'));
+      .pipe(gulp.dest('build/'));
 });
 
-gulp.task('rjs', function () {
-    return gulp.src('build/static/js/**/*.js')
-        .pipe(amdOptimize("main", {}))
-        .pipe(gulp.dest("build/static/js/"));
-});
-
-gulp.task('default', ['usemin', 'rjs']);
+gulp.task('default', ['usemin']);
---
+++ demo/static/js/home/main.js
@@ -0,0 +1,3 @@
+require(['home/index'], function(a) {
+    a.init();
+});
